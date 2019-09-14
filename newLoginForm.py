c5849a24cff5102bd6be99d4686595c9bb9cdb66
Sid Gupta <sgupta2@my.westga.edu>
2019-09-14 00:16:40 -0500

    added a comment form

---
+++ newLoginForm.py
@@ -0,0 +1,112 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'newLoginForm.ui'
+#
+# Created by: PyQt5 UI code generator 5.13.0
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(375, 406)
+        MainWindow.setStyleSheet("QPushButton {\n"
+"           color: red;\n"
+"           background-color: orange;\n"
+"           border-style: outset;\n"
+"           border-width: 2px;\n"
+"           border-radius: 15px;\n"
+"           border-color: black;\n"
+"           padding: 4px; \n"
+"         \n"
+"        }")
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.nameLabel = QtWidgets.QLabel(self.centralwidget)
+        self.nameLabel.setGeometry(QtCore.QRect(40, 40, 81, 21))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        font.setBold(True)
+        font.setWeight(75)
+        self.nameLabel.setFont(font)
+        self.nameLabel.setAlignment(QtCore.Qt.AlignLeading|QtCore.Qt.AlignLeft|QtCore.Qt.AlignVCenter)
+        self.nameLabel.setObjectName("nameLabel")
+        self.dateLabel = QtWidgets.QLabel(self.centralwidget)
+        self.dateLabel.setGeometry(QtCore.QRect(40, 90, 81, 21))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        font.setBold(True)
+        font.setWeight(75)
+        self.dateLabel.setFont(font)
+        self.dateLabel.setAlignment(QtCore.Qt.AlignLeading|QtCore.Qt.AlignLeft|QtCore.Qt.AlignVCenter)
+        self.dateLabel.setObjectName("dateLabel")
+        self.timeEdit = QtWidgets.QTimeEdit(self.centralwidget)
+        self.timeEdit.setGeometry(QtCore.QRect(197, 140, 118, 22))
+        self.timeEdit.setObjectName("timeEdit")
+        self.dateEdit = QtWidgets.QDateEdit(self.centralwidget)
+        self.dateEdit.setGeometry(QtCore.QRect(197, 90, 121, 22))
+        self.dateEdit.setObjectName("dateEdit")
+        self.timeLabel = QtWidgets.QLabel(self.centralwidget)
+        self.timeLabel.setGeometry(QtCore.QRect(40, 140, 81, 21))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        font.setBold(True)
+        font.setWeight(75)
+        self.timeLabel.setFont(font)
+        self.timeLabel.setAlignment(QtCore.Qt.AlignLeading|QtCore.Qt.AlignLeft|QtCore.Qt.AlignVCenter)
+        self.timeLabel.setObjectName("timeLabel")
+        self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit.setGeometry(QtCore.QRect(157, 40, 191, 20))
+        self.lineEdit.setObjectName("lineEdit")
+        self.textEdit = QtWidgets.QTextEdit(self.centralwidget)
+        self.textEdit.setGeometry(QtCore.QRect(40, 210, 311, 101))
+        self.textEdit.setObjectName("textEdit")
+        self.commentLabel = QtWidgets.QLabel(self.centralwidget)
+        self.commentLabel.setGeometry(QtCore.QRect(40, 180, 111, 16))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        font.setBold(True)
+        font.setWeight(75)
+        self.commentLabel.setFont(font)
+        self.commentLabel.setAlignment(QtCore.Qt.AlignLeading|QtCore.Qt.AlignLeft|QtCore.Qt.AlignVCenter)
+        self.commentLabel.setObjectName("commentLabel")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(150, 330, 111, 31))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        font.setBold(True)
+        font.setWeight(75)
+        self.pushButton.setFont(font)
+        self.pushButton.setObjectName("pushButton")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        self.dateEdit.setDate(QtCore.QDate.currentDate())
+        self.timeEdit.setTime(QtCore.QTime.currentTime())
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "Worm Label Tool"))
+        self.nameLabel.setText(_translate("MainWindow", "Name"))
+        self.dateLabel.setText(_translate("MainWindow", "Date"))
+        self.timeLabel.setText(_translate("MainWindow", "Time"))
+        self.commentLabel.setText(_translate("MainWindow", "Comment"))
+        self.pushButton.setText(_translate("MainWindow", "Submit"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
